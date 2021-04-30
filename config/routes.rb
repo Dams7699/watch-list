34c9ab7929108dd0984b0e432d62f95a8ce1a267
Hugo Daniel <daniel.hugo@protonmail.com>
2021-04-30 15:11:50 +0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
-  resources :articles, except: :index
+  root to: 'lists#index'
+  resources :lists, except: :index do
+    resources :bookmarks, [:new, :create]
+  end
+
+  resources :bookmarks, only: :destroy
 end
